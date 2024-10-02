8cdcbe8933e2e6b1c01a0a56277f11237a9cd3b8
Pragmatic <cryptopragmatic@protonmail.com>
2024-10-02 22:11:50 +0200

    start bot command

--- app/view_start_bot.go
+++ app/view_start_bot.go
@@ -1,23 +1,20 @@
 package app
 
 import (
-	"log"
-	"os/exec"
-
 	"github.com/go-macaron/session"
 	macaron "gopkg.in/macaron.v1"
 )
 
 func viewStartBot(sess session.Store, ctx *macaron.Context) string {
-	cmd := exec.Command("docker", "compose", "up", "-d")
-	cmd.Dir = "./bot1/"
+	// cmd := exec.Command("docker", "compose", "up", "-d")
+	// cmd.Dir = "./bot1/"
 
-	out, err := cmd.Output()
-	if err != nil {
-		loge(err)
-	}
+	// out, err := cmd.Output()
+	// if err != nil {
+	// 	loge(err)
+	// }
 
-	log.Println("Output: ", string(out))
+	// log.Println("Output: ", string(out))
 
 	return "Hello world"
 }
