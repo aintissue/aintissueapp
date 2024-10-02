1158beab59aef856c53e0d8cf032a8c7ebde3949
Pragmatic <cryptopragmatic@protonmail.com>
2024-10-02 23:56:05 +0200

    app - profile / email

--- app/view_profile.go
+++ app/view_profile.go
@@ -11,9 +11,23 @@ func viewProfile(sess session.Store, ctx *macaron.Context) {
 	ctx.HTML(200, "profile")
 }
 
-func viewProfileSave(prof ProfileForm, ctx *macaron.Context) {
+func viewProfileSave(prof ProfileForm, f *session.Flash, ctx *macaron.Context) {
 	log.Println(prof.Email)
-	ctx.HTML(200, "profile")
+	if len(prof.Email) == 0 {
+		f.Error("Email field is required.")
+		ctx.Redirect("/profile")
+		return
+	} else {
+		u := ctx.Data["User"].(*User)
+		u.Email = prof.Email
+		err := db.Save(u).Error
+		if err != nil {
+			loge(err)
+		}
+		f.Success("Email saved successfully.")
+		ctx.Redirect("/profile")
+		return
+	}
 }
 
 type ProfileForm struct {
